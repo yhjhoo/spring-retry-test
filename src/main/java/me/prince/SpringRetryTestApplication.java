65d356f199155a142dc8941d55642cdacdca1fc5
huajieyang <huajieyang@dbs.com>
2017-03-09 11:47:07 +0800

    add feign client sample

--- src/main/java/me/prince/SpringRetryTestApplication.java
+++ src/main/java/me/prince/SpringRetryTestApplication.java
@@ -1,5 +1,6 @@
 package me.prince;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,17 +8,19 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.EnableRetry;
-import org.springframework.retry.annotation.Retryable;
 import org.springframework.retry.backoff.FixedBackOffPolicy;
 import org.springframework.retry.policy.AlwaysRetryPolicy;
-import org.springframework.retry.policy.SimpleRetryPolicy;
 import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -25,6 +28,8 @@ import java.util.Date;
 
 @SpringBootApplication
 @EnableRetry
+@EnableFeignClients
+@EnableCircuitBreaker
 public class SpringRetryTestApplication {
 
     @Autowired
@@ -60,16 +65,30 @@ class WelcomeController{
     @Autowired
     private ServiceCall serviceCall;
 
+    @Autowired
+    private FeignServiceTest feignServiceTest;
+
+    @Autowired
+    private UaaAdminFeignTest uaaAdminFeignTest;
+
+    @GetMapping(value = "/feign", produces = "application/json")
+    public String feignService(){
+        return feignServiceTest.version() ;
+    }
+
+    @GetMapping(value = "/feign2", produces = "application/json")
+    public String uaaAdminFeignTest(){
+        return uaaAdminFeignTest.version() ;
+    }
+
     @GetMapping("/index")
     public String callService(){
-        serviceCall.callService();
-
-        return "call Service";
+        return serviceCall.callService();
     }
 
 }
 
-@Component
+//@Component
 class RetryTest implements ApplicationRunner{
 
     @Autowired
@@ -92,16 +111,47 @@ class RetryTest implements ApplicationRunner{
 
 }
 
+@FeignClient(name="test", url = "http://demo.url")
+interface FeignServiceTest{
+    @RequestMapping(value = "/info", method = RequestMethod.GET)
+    String version();
+}
+
+
+@FeignClient(name="test", url = "http://demo.url", fallback = UaaAdminFeignTestFallBack.class)
+interface UaaAdminFeignTest{
+    @RequestMapping(value = "/health", method = RequestMethod.GET)
+    String version();
+}
+
+@Component
+class UaaAdminFeignTestFallBack implements UaaAdminFeignTest {
+
+    @Override
+    public String version() {
+        return "I am die UaaAdminFeignTestFallBack !";
+    }
+}
+
 
 @Service
 class ServiceCall{
     private RestTemplate restTemplate = new RestTemplate();
     private Log logger = LogFactory.getLog(this.getClass());
 
-    @Retryable(backoff = @Backoff(delay = 3000), maxAttempts = 10)
-    public void callService(){
+//    @Retryable(backoff = @Backoff(delay = 3000), maxAttempts = 10)
+    @HystrixCommand(fallbackMethod = "fallBack")
+    public String callService(){
         logger.info("++++++++++++++++++++++++++++++++++++++++++++++++++" + new Date() );
         String str = restTemplate.getForObject("http://demo.url/health",String.class);
         logger.info(str);
+        return str;
+    }
+
+    static String fallBack(){
+        return "I am die ServiceCall";
     }
 }
+
+
+
